7c772b273af88fd6bdd9ea20d257f97959a16869
Yradouani <yasmine.radouani@outlook.fr>
2023-01-02 15:33:54 +0100

    added data file

---
+++ data/data.js
@@ -0,0 +1,72 @@
+let menus = [
+    {
+        id: 1,
+        name: '',
+        picture: './assets/',
+        type: 'main',
+        description: '',
+        price: '',
+    },
+    {
+        id: 2,
+        name: '',
+        picture: '',
+        description: '',
+        price: './assets/'
+    },
+    {
+        id: 3,
+        name: '',
+        picture: './assets/',
+        description: '',
+        price: ''
+    },
+    {
+        id: 4,
+        name: 'Paëlla',
+        picture: './assets/rice-dish.jpg',
+        type: 'main',
+        description: 'Plat traditionnel espagnol, convivial et savoureux. Elle se compose de riz cuit à la vapeur, de légumes, de poisson ou de viande et de fruits de mer',
+        price: ''
+    },
+    {
+        id: 5,
+        name: 'Hamburger maison',
+        picture: './assets/burgers.jpg',
+        type: 'main',
+        description: 'Sandwich iconique, composé d\'un pain rond et moelleux, d\'une viande hachée grillée et d\'une variété de garnitures. Le pain est coupé en deux et grillé, puis garni de laitue, de tomate, de oignon et de mayonnaise.',
+        price: '9.99'
+    },
+    {
+        id: 6,
+        name: 'Pizza aux anchois',
+        picture: './assets/plate.jpg',
+        type: 'main',
+        description: 'Composée d\'un disque de pâte à pizza croustillant et doré, recouvert de tomates pelées et coupées en rondelles, de fromage fondu et de fines tranches d\'anchois.',
+        price: '8.99'
+    },
+    {
+        id: 7,
+        name: 'Tiramisu',
+        picture: './assets/cake.jpg',
+        type: 'dessert',
+        description: 'Découvrez ce formidable dessert italien composé de biscuits roulés dans un café tradition de haute qualité, recouvert d\'une crème onctueuse et légèrement sucrée',
+        price: '6.99'
+    },
+    {
+        id: 8,
+        name: 'Crème brulée',
+        picture: './assets/creme-brulee.jpg',
+        type: 'dessert',
+        description: 'Déguster notre délicieuse crème anglaise bien ferme et légèrement dorée à base de crumble croustillant au sucre caramélisé',
+        price: '7.99'
+    },
+    {
+        id: 9,
+        name: 'Pancakes aux framboises',
+        picture: './assets/pancakes.jpg',
+        type: 'dessert',
+        description: 'Crêpes épaisses et moelleuses, cuites à la poêle, garnies de sucre, de beurre et de sirop d\'érable, recouvertes de framboises fraiches',
+        price: ''
+    },
+]
