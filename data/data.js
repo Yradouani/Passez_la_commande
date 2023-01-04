bbde0d127584566d86048f5dd9802cb81b15596a
Yradouani <yasmine.radouani@outlook.fr>
2023-01-04 21:36:25 +0100

    cards display

--- data/data.js
+++ data/data.js
@@ -4,6 +4,8 @@ export const menus = [
         name: 'Salade de crudités',
         picture: './assets/salade.jpg',
         type: 'starter',
+        isLiked: false,
+        isBestSeller: true,
         description: 'Salade composée de carottes râpées, coupées en fines tranches. Elle est assaisonnée avec une vinaigrette ou une autre sauce au choix, et est garnie de divers ingrédients tels que des noix, des graines, de la feta ou du fromage râpé et des feuilles de menthe.',
         price: '5.99',
     },
@@ -12,6 +14,8 @@ export const menus = [
         name: 'Pain de viande',
         picture: './assets/Pain.jpg',
         type: 'starter',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Le pain de viande est un plat de viande hachée cuit au four et qui est généralement servi en tranches. Il est préparé avec de la viande hachée, du riz, des oignons, des œufs et des épices.',
         price: '4.99'
     },
@@ -20,7 +24,9 @@ export const menus = [
         name: 'Trempette à nachos',
         picture: './assets/nachos.jpg',
         type: 'starter',
-        description: 'chips de maïs croustillantes recouvertes de fromage fondu et de diverses garnitures, comme de la viande hachée, des haricots, des tomates, des oignons, des poivrons, du guacamole et de la crème sure.',
+        isLiked: false,
+        isBestSeller: true,
+        description: 'Chips de maïs croustillantes recouvertes de fromage fondu et de diverses garnitures, comme de la viande hachée, des haricots, des tomates, des oignons, des poivrons, du guacamole et de la crème sure.',
         price: '4.99'
     },
     {
@@ -28,6 +34,8 @@ export const menus = [
         name: 'Paëlla',
         picture: './assets/rice-dish.jpg',
         type: 'main',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Plat traditionnel espagnol, convivial et savoureux. Elle se compose de riz cuit à la vapeur, de légumes, de poisson ou de viande et de fruits de mer',
         price: '9.99'
     },
@@ -36,6 +44,8 @@ export const menus = [
         name: 'Hamburger maison',
         picture: './assets/burgers.jpg',
         type: 'main',
+        isLiked: false,
+        isBestSeller: true,
         description: 'Sandwich iconique, composé d\'un pain rond et moelleux, d\'une viande hachée grillée et d\'une variété de garnitures. Le pain est coupé en deux et grillé, puis garni de laitue, de tomate, de oignon et de mayonnaise.',
         price: '9.99'
     },
@@ -44,6 +54,8 @@ export const menus = [
         name: 'Pizza aux anchois',
         picture: './assets/plate.jpg',
         type: 'main',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Composée d\'un disque de pâte à pizza croustillant et doré, recouvert de tomates pelées et coupées en rondelles, de fromage fondu et de fines tranches d\'anchois.',
         price: '8.99'
     },
@@ -52,6 +64,8 @@ export const menus = [
         name: 'Tiramisu',
         picture: './assets/cake.jpg',
         type: 'dessert',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Découvrez ce formidable dessert italien composé de biscuits roulés dans un café tradition de haute qualité, recouvert d\'une crème onctueuse et légèrement sucrée',
         price: '6.99'
     },
@@ -60,6 +74,8 @@ export const menus = [
         name: 'Crème brulée',
         picture: './assets/creme-brulee.jpg',
         type: 'dessert',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Déguster notre délicieuse crème anglaise bien ferme et légèrement dorée à base de crumble croustillant au sucre caramélisé',
         price: '7.99'
     },
@@ -68,6 +84,8 @@ export const menus = [
         name: 'Pancakes aux framboises',
         picture: './assets/pancakes.jpg',
         type: 'dessert',
+        isLiked: false,
+        isBestSeller: true,
         description: 'Crêpes épaisses et moelleuses, cuites à la poêle, garnies de sucre, de beurre et de sirop d\'érable, recouvertes de framboises fraiches',
         price: '5.99'
     },
@@ -76,6 +94,8 @@ export const menus = [
         name: 'jus d\'orange',
         picture: './assets/jus.jpeg',
         type: 'drink',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Jus d’orange pressé fait maison avec des oranges espagnoles, bios et juteuses.',
         price: '3.99'
     },
@@ -84,6 +104,8 @@ export const menus = [
         name: 'smoothie',
         picture: './assets/smoothie.jpg',
         type: 'drink',
+        isLiked: false,
+        isBestSeller: false,
         description: 'Boisson fraîche et onctueuse faite en mélangeant des fruits, des légumes et des ingrédients tels que des glaçons, du lait ou du yogourt.',
         price: '4.99'
     },
@@ -92,6 +114,8 @@ export const menus = [
         name: 'chocolat chaud',
         picture: './assets/chocolate.jpg',
         type: 'drink',
+        isLiked: false,
+        isBestSeller: true,
         description: 'Boisson chaude et onctueuse préparée en faisant fondre du chocolat en poudre et des morceaux de chocolat dans du lait chaud',
         price: '3.99'
     },
--- home.js
+++ home.js
@@ -1,10 +1,59 @@
 import { menus } from "./data/data.js";
-// let menus = require("./data/data");
 
+let drink = document.querySelector("#drink");
 let starter = document.querySelector("#starter");
+let main = document.querySelector("#main");
+let dessert = document.querySelector("#dessert");
 
 menus.forEach(menu => {
-    console.log(menu)
-    // starter.innerHTML += `<div>`
+    if (menu.type === "drink") {
+        drink.innerHTML += `
+        <div class="fich">
+            <img src=${menu.picture} class="img" alt=${menu.name}>
+            <h3 class="title">${menu.name}</h3>
+            <p class="description">${menu.description}</p>
+            <div class="flex">
+                <p class="price">${menu.price}€</p>
+                <button class="btnCommander">Commander</button>
+            </div>
+        </div> 
+        `
+    } else if (menu.type === "starter"){
+        starter.innerHTML += `
+        <div class="fich">
+            <img src=${menu.picture} class="img" alt=${menu.name}>
+            <h3 class="title">${menu.name}</h3>
+            <p class="description">${menu.description}</p>
+            <div class="flex">
+                <p class="price">${menu.price}€</p>
+                <button class="btnCommander">Commander</button>
+            </div>
+        </div> 
+        `
+    } else if (menu.type === "main"){
+        main.innerHTML += `
+        <div class="fich">
+            <img src=${menu.picture} class="img" alt=${menu.name}>
+            <h3 class="title">${menu.name}</h3>
+            <p class="description">${menu.description}</p>
+            <div class="flex">
+                <p class="price">${menu.price}€</p>
+                <button class="btnCommander">Commander</button>
+            </div>
+        </div> 
+        `
+    } else {
+        dessert.innerHTML += `
+        <div class="fich">
+            <img src=${menu.picture} class="img" alt=${menu.name}>
+            <h3 class="title">${menu.name}</h3>
+            <p class="description">${menu.description}</p>
+            <div class="flex">
+                <p class="price">${menu.price}€</p>
+                <button class="btnCommander">Commander</button>
+            </div>
+        </div> 
+        `
+    }
 })
 
