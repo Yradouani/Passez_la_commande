e8032ee417d75e933e7513005eb398bbb1a5f6d7
Yradouani <yasmine.radouani@outlook.fr>
2023-01-04 15:31:01 +0100

    updated recap page

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ export const menus = [
         name: 'Salade de crudités',
         picture: './assets/salade.jpg',
         type: 'starter',
-        description: '',
+        description: 'Salade composée de carottes râpées, coupées en fines tranches. Elle est assaisonnée avec une vinaigrette ou une autre sauce au choix, et est garnie de divers ingrédients tels que des noix, des graines, de la feta ou du fromage râpé et des feuilles de menthe.',
         price: '5.99',
     },
     {
@@ -12,16 +12,16 @@ export const menus = [
         name: 'Pain de viande',
         picture: './assets/Pain.jpg',
         type: 'starter',
-        description: '',
+        description: 'Le pain de viande est un plat de viande hachée cuit au four et qui est généralement servi en tranches. Il est préparé avec de la viande hachée, du riz, des oignons, des œufs et des épices.',
         price: '4.99'
     },
     {
         id: 3,
-        name: '',
-        picture: './assets/',
+        name: 'Trempette à nachos',
+        picture: './assets/nachos.jpg',
         type: 'starter',
-        description: '',
-        price: ''
+        description: 'chips de maïs croustillantes recouvertes de fromage fondu et de diverses garnitures, comme de la viande hachée, des haricots, des tomates, des oignons, des poivrons, du guacamole et de la crème sure.',
+        price: '4.99'
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ export const menus = [
         picture: './assets/rice-dish.jpg',
         type: 'main',
         description: 'Plat traditionnel espagnol, convivial et savoureux. Elle se compose de riz cuit à la vapeur, de légumes, de poisson ou de viande et de fruits de mer',
-        price: ''
+        price: '9.99'
     },
     {
         id: 5,
@@ -70,5 +70,29 @@ export const menus = [
         type: 'dessert',
         description: 'Crêpes épaisses et moelleuses, cuites à la poêle, garnies de sucre, de beurre et de sirop d\'érable, recouvertes de framboises fraiches',
         price: '5.99'
+    },
+    {
+        id: 10,
+        name: 'jus d\'orange',
+        picture: './assets/jus.jpeg',
+        type: 'drink',
+        description: 'Jus d’orange pressé fait maison avec des oranges espagnoles, bios et juteuses.',
+        price: '3.99'
+    },
+    {
+        id: 11,
+        name: 'smoothie',
+        picture: './assets/smoothie.jpg',
+        type: 'drink',
+        description: 'Boisson fraîche et onctueuse faite en mélangeant des fruits, des légumes et des ingrédients tels que des glaçons, du lait ou du yogourt.',
+        price: '4.99'
+    },
+    {
+        id: 12,
+        name: 'chocolat chaud',
+        picture: './assets/chocolate.jpg',
+        type: 'drink',
+        description: 'Boisson chaude et onctueuse préparée en faisant fondre du chocolat en poudre et des morceaux de chocolat dans du lait chaud',
+        price: '3.99'
     },
 ]
