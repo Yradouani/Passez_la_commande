6b6d51f80125cecf236a2e1f7361c529820f1c54
Yradouani <yasmine.radouani@outlook.fr>
2023-01-09 14:07:00 +0100

    model card change

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let navbar = document.querySelector(".bar");
+let mobileNavbar = document.querySelector("#mobile_navbar")
+
+navbar.addEventListener("click", () => {
+    mobileNavbar.style.display = "block"
+})
--- recapitulatif.js
+++ recapitulatif.js
@@ -3,26 +3,33 @@ let orderDishes = JSON.parse(localStorage.getItem("orderDishes"));
 let tableContent = document.querySelector("tbody");
 let totalPriceOfOneDishe;
 let totalContent = document.querySelector("#total");
+let validateButton = document.querySelector("#validateButton");
 let total = 0;
 
 function display() {
-    tableContent.innerHTML = ``
-    for (let i = 0; i < orderDishes.length; i++) {
-        let dishId = orderDishes[i].id;
-        for (let j = 0; j < menus.length; j++) {
-            if (dishId === menus[j].id) {
-                totalPriceOfOneDishe = parseFloat((orderDishes[i].quantity) * (menus[j].price));
-                totalPriceOfOneDishe = Math.round(totalPriceOfOneDishe * 100) / 100
-                tableContent.innerHTML += `
-                    <tr>
-                        <td>${menus[j].name}</td>
-                        <td><i class="fa-solid fa-minus minus ${menus[j].id}"></i><span class="quantity"> ${orderDishes[i].quantity} </span><i class="fa-solid fa-plus plus"></i></td>
-                        <td>${menus[j].price}€</td>
-                        <td class="totalPriceOfOneDishe">${totalPriceOfOneDishe}€</td>
-                        <td><i class="fa-solid fa-trash-can trash" id=${menus[j].id}></i></td>
-                    </tr>
-                `
-                total += totalPriceOfOneDishe;
+    console.log(orderDishes)
+    if (orderDishes == []) {
+        // tableContent.innerHTML = ``
+        tableContent.innerHTML += `<p>Votre panier est vide</p>`
+    } else {
+        tableContent.innerHTML = ``
+        for (let i = 0; i < orderDishes.length; i++) {
+            let dishId = orderDishes[i].id;
+            for (let j = 0; j < menus.length; j++) {
+                if (dishId === menus[j].id) {
+                    totalPriceOfOneDishe = parseFloat((orderDishes[i].quantity) * (menus[j].price));
+                    totalPriceOfOneDishe = Math.round(totalPriceOfOneDishe * 100) / 100
+                    tableContent.innerHTML += `
+                        <tr>
+                            <td>${menus[j].name}</td>
+                            <td><i class="fa-solid fa-minus minus ${menus[j].id}"></i><span class="quantity"> ${orderDishes[i].quantity} </span><i class="fa-solid fa-plus plus"></i></td>
+                            <td>${menus[j].price}€</td>
+                            <td class="totalPriceOfOneDishe">${totalPriceOfOneDishe}€</td>
+                            <td><i class="fa-solid fa-trash-can trash" id=${menus[j].id}></i></td>
+                        </tr>
+                    `
+                    total += totalPriceOfOneDishe;
+                }
             }
         }
     }
@@ -89,3 +96,6 @@ for (let i = 0; i < trashButtons.length; i++) {
     })
 }
 
+validateButton.addEventListener("click", () => {
+    document.location.href = "./facturation.html";
+})
