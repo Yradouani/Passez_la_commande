83824b29f458198aac4ab6b64bf617821c7b2877
Yradouani <yasmine.radouani@outlook.fr>
2023-01-09 15:22:43 +0100

    navbar mobile version added

--- navbar.js
+++ navbar.js
@@ -1,6 +1,11 @@
 let navbar = document.querySelector(".bar");
-let mobileNavbar = document.querySelector("#mobile_navbar")
+let mobileNavbar = document.querySelector("#mobile_navbar");
+let closeNavbar = document.querySelector(".cross")
 
 navbar.addEventListener("click", () => {
     mobileNavbar.style.display = "block"
+})
+
+closeNavbar.addEventListener("click", () => {
+    mobileNavbar.style.display = "none"
 })
